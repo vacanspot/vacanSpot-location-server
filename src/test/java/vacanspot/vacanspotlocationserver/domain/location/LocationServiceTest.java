71c8bb45db1c05aa3bec2ba441877fedd5fa1b52
jepark <psjepark@gmail.com>
2023-09-02 16:50:24 +0900

    feat : 좌표 주소 변환 api 테스트 코드 작성

---
+++ src/test/java/vacanspot/vacanspotlocationserver/domain/location/LocationServiceTest.java
@@ -0,0 +1,29 @@
+package vacanspot.vacanspotlocationserver.domain.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import vacanspot.vacanspotlocationserver.domain.location.dto.request.KakaoLocationRequest;
+import vacanspot.vacanspotlocationserver.domain.location.dto.response.KakaoLocationResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LocationServiceTest {
+
+    @Autowired
+    private LocationService locationService;
+
+    @Test
+    public void test() {
+        KakaoLocationRequest kakaoLocationRequest = new KakaoLocationRequest(
+                "127.423084873712",
+                "37.0789561558879",
+                "WGS84"
+        );
+        KakaoLocationResponse kakaoLocation = locationService.getKakaoLocation(kakaoLocationRequest);
+
+        System.out.println(kakaoLocation);
+    }
+
+}
