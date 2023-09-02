4365090245f485c7e6be918a1587b531d0030cd4
jepark <psjepark@gmail.com>
2023-09-02 14:00:33 +0900

    feat : location-server repository 생성

---
+++ src/main/java/vacanspot/vacanspotlocationserver/VacanspotLocationServerApplication.java
@@ -0,0 +1,13 @@
+package vacanspot.vacanspotlocationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanspotLocationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacanspotLocationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vacanspot/vacanspotlocationserver/VacanspotLocationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vacanspot.vacanspotlocationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanspotLocationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
